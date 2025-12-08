43e2b4b36beabf1cece02a4f739b9033090c3fc6
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-12-09 01:58:15 +0900

    feat: 임시 서명 생성 도구 추가

---
+++ server/utils/security.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto');
+
+// 재생 공격 방지용 Nonce 생성
+exports.generateNonce = () => crypto.randomBytes(16).toString('hex');
+
+// HMAC-SHA256 서명 생성 (OpenSSL 기반)
+exports.signPayload = (payload, secret) => {
+    return crypto.createHmac('sha256', secret).update(payload).digest('hex');
+};
