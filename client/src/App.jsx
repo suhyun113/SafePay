b3794c0321598fa6ab9883fe1d00b36f2b27652b
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-12-09 03:13:24 +0900

    feat: 인증 라우팅 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DualPanel from "./pages/DualPanel";
 import SafeFlow from "./pages/SafeFlow";
 import AttackFlow from "./pages/AttackFlow";
+import Auth from "./pages/Auth";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DualPanel />} />
-        <Route path="/auth" element={<Auth />} />
         <Route path="/safe" element={<SafeFlow />} />
         <Route path="/attack" element={<AttackFlow />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
