99f3f5d100a6bea3873d43d413caa614c05820b6
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-12-09 02:21:52 +0900

    feat: 결제 모델 생성

---
+++ server/models/orderModel.js
@@ -0,0 +1,8 @@
+const db = require('../config/db');
+
+exports.createOrder = async (userId, item, amount, nonce, signature) => {
+    const sql = `
+        INSERT INTO orders (user_id, item, amount, nonce, signature)
+        VALUES (?, ?, ?, ?, ?)`;
+    await db.execute(sql, [userId, item, amount, nonce, signature]);
+};
