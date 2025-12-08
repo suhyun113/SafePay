d932c6cc72f437814ae5bb3ff11eaa064925a0ba
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-12-09 03:05:22 +0900

    AttackFlow 화면 출력 추가

--- client/src/pages/AttackFlow.jsx
+++ client/src/pages/AttackFlow.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import api from "../api/axios";
 
 export default function AttackFlow() {
-  const testCSRF = async () => {
+  const [log, setLog] = useState("");
+
+  const csrf = async () => {
     const res = await api.post("/attack/csrf");
-    console.log(res.data);
+    setLog(JSON.stringify(res.data, null, 2));
+  };
+
+  const replay = async () => {
+    const res = await api.post("/attack/replay");
+    setLog(JSON.stringify(res.data, null, 2));
   };
 
   return (
     <div>
       <h1>Attack Simulation</h1>
-      <button onClick={testCSRF}>Simulate CSRF Attack</button>
+      <button onClick={csrf}>CSRF Attack</button>
+      <button onClick={replay}>Replay Attack</button>
+
+      <pre style={{ background: "#111", padding: 20, color: "red" }}>
+        {log}
+      </pre>
     </div>
   );
 }
